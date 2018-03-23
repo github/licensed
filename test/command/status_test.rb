04ca5669bc7494c69b64369c81266cb6e31c5473
Jon Ruskin <jon.ruskin@gmail.com>
2018-03-22 23:16:14 -0700

    use lazy-load verifier command

--- test/command/status_test.rb
+++ test/command/status_test.rb
@@ -4,6 +4,7 @@ require "test_helper"
 describe Licensed::Command::Status do
   let(:config) { Licensed::Configuration.new }
   let(:source) { TestSource.new }
+  let(:verifier) { Licensed::Command::Status.new(config) }
 
   before do
     config.apps.each do |app|
@@ -14,35 +15,34 @@ describe Licensed::Command::Status do
     config.ui.silence do
       Licensed::Command::Cache.new(config.dup).run(force: true)
     end
-    @verifier = Licensed::Command::Status.new(config)
   end
 
   it "warns if license is not allowed" do
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     assert_match(/license needs reviewed: mit/, out)
   end
 
   it "does not warn if license is allowed" do
     config.allow "mit"
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     refute_match(/license needs reviewed: mit/, out)
   end
 
   it "does not warn if dependency is ignored" do
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     assert_match(/dependency.txt/, out)
 
     config.ignore "type" => "test", "name" => "dependency"
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     refute_match(/dependency.txt/, out)
   end
 
   it "does not warn if dependency is reviewed" do
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     assert_match(/dependency/, out)
 
     config.review "type" => "test", "name" => "dependency"
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     refute_match(/dependency/, out)
   end
 
@@ -52,19 +52,19 @@ describe Licensed::Command::Status do
     license.text = ""
     license.save(filename)
 
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     assert_match(/missing license text/, out)
   end
 
   it "warns if versions do not match" do
-    @verifier.app_dependencies(config.apps.first).first["version"] = "nope"
-    out, _ = capture_io { @verifier.run }
+    verifier.app_dependencies(config.apps.first).first["version"] = "nope"
+    out, _ = capture_io { verifier.run }
     assert_match(/cached license data out of date/, out)
   end
 
   it "warns if cached license data missing" do
     FileUtils.rm config.cache_path.join("test/dependency.txt")
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     assert_match(/cached license data missing/, out)
   end
 
@@ -72,16 +72,16 @@ describe Licensed::Command::Status do
     FileUtils.rm config.cache_path.join("test/dependency.txt")
     config.ignore "type" => "test", "name" => "dependency"
 
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     refute_match(/dependency/, out)
   end
 
   it "does not include ignored dependencies in dependency counts" do
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     count = out.match(/(\d+) dependencies checked/)[1].to_i
 
     config.ignore "type" => "test", "name" => "dependency"
-    out, _ = capture_io { @verifier.run }
+    out, _ = capture_io { verifier.run }
     ignored_count = out.match(/(\d+) dependencies checked/)[1].to_i
     assert_equal count - 1, ignored_count
   end
@@ -104,7 +104,7 @@ describe Licensed::Command::Status do
     let(:config) { Licensed::Configuration.new("apps" => apps) }
 
     it "verifies dependencies for all apps" do
-      out, _ = capture_io { @verifier.run }
+      out, _ = capture_io { verifier.run }
       config.apps.each do |app|
         assert_match(/Checking licenses for #{app['name']}/, out)
       end
@@ -117,7 +117,7 @@ describe Licensed::Command::Status do
 
     it "changes the current directory to app.source_path while running" do
       source.dependencies_hook = -> { assert_equal fixtures, Dir.pwd }
-      capture_io { @verifier.run }
+      capture_io { verifier.run }
     end
   end
 end
