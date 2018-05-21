5097332627ac3bd5eaea2ba32641484742b0358e
Will Roden <willabides@github.com>
2018-05-20 19:16:20 -0500

    Add failing test for vendored bits of the go standard library

--- test/fixtures/go/src/test/test.go
+++ test/fixtures/go/src/test/test.go
@@ -3,9 +3,11 @@ package test
 import (
   lru "github.com/hashicorp/golang-lru"
   ctx "github.com/gorilla/context"
+  "golang.org/x/text/unicode/norm"
 )
 
 func main() { 
   lru.New(1)
   ctx.Purge(0)
+  _ = norm.Version
 }
