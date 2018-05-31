89595154b5ceda83765c80c3954466b789bdde11
Avinash Sridhar <sridharavinash@github.com>
2018-05-31 16:09:38 -0400

    lint

--- test/source/pip_test.rb
+++ test/source/pip_test.rb
@@ -5,8 +5,7 @@ require "tmpdir"
 if Licensed::Shell.tool_available?("pip")
   describe Licensed::Source::Pip do
     let (:fixtures)  { File.expand_path("../../fixtures/pip", __FILE__) }
-    let (:config)   { Licensed::Configuration.new(
-                        "python" => {"virtual_env_dir" => "test/fixtures/pip/venv"})}
+    let (:config)   { Licensed::Configuration.new("python" => {"virtual_env_dir" => "test/fixtures/pip/venv"}) }
     let (:source)   { Licensed::Source::Pip.new(config) }
 
     describe "enabled?" do
