e1c48993818b1dce5df3e329d5d1d22046494e12
Jon Ruskin <jon.ruskin@gmail.com>
2018-07-10 14:15:30 -0700

    remove remote fetching

--- lib/licensed.rb
+++ lib/licensed.rb
@@ -17,32 +17,3 @@ require "licensed/command/cache"
 require "licensed/command/status"
 require "licensed/command/list"
 require "licensed/ui/shell"
-require "octokit"
-
-module Licensed
-  class << self
-    attr_accessor :use_github
-  end
-
-  self.use_github = true
-
-  GITHUB_URL = %r{\Ahttps://github.com/([a-z0-9]+(-[a-z0-9]+)*/(\w|\.|\-)+)}
-  LICENSE_CONTENT_TYPE = "application/vnd.github.drax-preview+json"
-
-  # Load license content from a GitHub url.  Returns nil if the url does not point
-  # to a GitHub repository, or if the license content is not found
-  def self.from_github(url)
-    return unless use_github && match = GITHUB_URL.match(url)
-
-    license_url = Octokit::Repository.path(match[1]) + "/license"
-    response = octokit.get license_url, accept: LICENSE_CONTENT_TYPE
-    content = Base64.decode64(response["content"]).force_encoding("UTF-8")
-    Licensee::ProjectFiles::LicenseFile.new(content, response["name"])
-  rescue Octokit::NotFound
-    nil
-  end
-
-  def self.octokit
-    @octokit ||=  Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
-  end
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,21 +4,6 @@ require "minitest/autorun"
 require "licensed"
 require "English"
 
-# Make sure this doesn't get recorded in VCR responses
-ENV["GITHUB_TOKEN"] = nil
-
-require "vcr"
-VCR.configure do |config|
-  config.cassette_library_dir = File.expand_path("../fixtures/vcr", __FILE__)
-  config.hook_into :webmock
-end
-
-Minitest::Spec.class_eval do
-  before do
-    Licensed.use_github = false
-  end
-end
-
 class TestSource
   attr_accessor :dependencies_hook
 
