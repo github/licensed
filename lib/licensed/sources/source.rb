2e6218d500072b53b2d63f19bf3998789042db8d
Jon Ruskin <jon.ruskin@gmail.com>
2018-12-26 09:33:36 -0700

    add documentation

--- lib/licensed/sources/source.rb
+++ lib/licensed/sources/source.rb
@@ -12,9 +12,12 @@ module Licensed
       class << self
         attr_reader :sources
         def inherited(klass)
+          # add child source classes are defined,
+          # add them to the known sources list
           (@sources ||= []) << klass
         end
 
+        # Returns the source name as the snake cased class name
         def type
           self.name.split(/::/)
                    .last
@@ -24,30 +27,38 @@ module Licensed
         end
       end
 
+      # all sources have a configuration
       attr_accessor :config
 
       def initialize(configuration)
         @config = configuration
       end
 
+      # Returns whether a source is enabled based on the environment in which licensed is run
+      # Defaults to false.
       def enabled?
         false
       end
 
+      # Returns all dependencies that should be evaluated.
+      # Excludes ignored dependencies.
       def dependencies
         cached_dependencies.reject { |d| ignored?(d) }
       end
 
+      # Enumerate all source dependencies.  Must be implemented by each source class.
       def enumerate_dependencies
         raise DependencyEnumerationNotImplementedError
       end
 
       private
 
+      # Returns a cached list of dependencies
       def cached_dependencies
         @dependencies ||= enumerate_dependencies.compact
       end
 
+      # Returns whether a dependency is ignored in the configuration.
       def ignored?(dependency)
         config.ignored?("type" => self.class.type, "name" => dependency["name"])
       end
