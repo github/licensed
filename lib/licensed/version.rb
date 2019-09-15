64aeccc9bbb90d079389e942b6020e5715937dee
Jon Ruskin <jon.ruskin@gmail.com>
2019-09-15 13:14:30 -0700

    release 2.4.0

--- lib/licensed/version.rb
+++ lib/licensed/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Licensed
-  VERSION = "2.3.2".freeze
+  VERSION = "2.4.0".freeze
 
   def self.previous_major_versions
     major_version = Gem::Version.new(Licensed::VERSION).segments.first
