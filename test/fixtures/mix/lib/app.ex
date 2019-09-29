463ee7888668a3e4b79087683e407a8a8cb76d10
Bruce Williams <brwcodes@gmail.com>
2019-09-29 15:34:11 -0700

    Add script/source-setup/mix and fixtures

---
+++ test/fixtures/mix/lib/app.ex
@@ -0,0 +1,18 @@
+defmodule App do
+  @moduledoc """
+  Documentation for App.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> App.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fixtures/mix/mix.exs
@@ -0,0 +1,30 @@
+defmodule App.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :mix,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:plug, "~> 1.8.3"},
+      {:mox, "~> 0.5.1", only: [:test]}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
