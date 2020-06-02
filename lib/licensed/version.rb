220aac5b2ea18d50f1af9d85f10756faa95ec35b
Jon Ruskin <jonabc@github.com>
2020-06-02 09:38:43 -0700

    release 2.11.0

--- lib/licensed/version.rb
+++ lib/licensed/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Licensed
-  VERSION = "2.10.0".freeze
+  VERSION = "2.11.0".freeze
 
   def self.previous_major_versions
     major_version = Gem::Version.new(Licensed::VERSION).segments.first
