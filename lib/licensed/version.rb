ad3a0ed00453131da6b518b8794a7c4ad3eb8031
Jon Ruskin <jonabc@github.com>
2022-02-07 19:47:49 -0700

    release 3.4.4
    ## 3.4.4
    
    2022-02-07
    
    ### Fixed
    
    - The npm and pip sources have better protection from strings causing crashes in `Hash#dig` (https://github.com/github/licensed/pull/450)

--- lib/licensed/version.rb
+++ lib/licensed/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Licensed
-  VERSION = "3.4.3".freeze
+  VERSION = "3.4.4".freeze
 
   def self.previous_major_versions
     major_version = Gem::Version.new(Licensed::VERSION).segments.first
