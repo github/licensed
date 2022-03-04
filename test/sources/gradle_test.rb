0ed9a89b9204f9c06e03aa886bb0dfb503b2f5f5
civitaspo <civitaspo@gmail.com>
2022-03-05 01:58:18 +0900

    Add a gradle multi-project test
    Signed-off-by: civitaspo <civitaspo@gmail.com>

--- test/sources/gradle_test.rb
+++ test/sources/gradle_test.rb
@@ -51,10 +51,49 @@ describe Licensed::Sources::Gradle do
 end
 
 describe Licensed::Sources::Gradle::Dependency do
+  let(:config) { Licensed::AppConfiguration.new({ "source_path" => Dir.pwd }) }
+  let(:source) { Licensed::Sources::Gradle.new(config) }
+
   context "with a simple gradle project" do
     let(:fixtures) { File.expand_path("../../fixtures/gradle/simple", __FILE__) }
-    let(:config) { Licensed::AppConfiguration.new({ "source_path" => Dir.pwd }) }
-    let(:source) { Licensed::Sources::Gradle.new(config) }
+
+    it "returns the dependency license" do
+      Dir.chdir fixtures do
+        dep = source.dependencies.detect { |d| d.name == "io.netty:netty-all" }
+        assert dep
+        assert_equal "apache-2.0", dep.license.key
+
+        license = dep.record.licenses.find { |l| l.text =~ /Apache License/ }
+        assert license
+        assert_equal ["https://www.apache.org/licenses/LICENSE-2.0"], license.sources
+      end
+    end
+
+    it "cleans up grade licenses csv content" do
+      Dir.chdir fixtures do
+        dep = source.dependencies.detect { |d| d.name == "io.netty:netty-all" }
+        # load the dependency record, pulling license files
+        dep.record
+
+        refute Pathname.pwd.join(Licensed::Sources::Gradle::GRADLE_LICENSES_PATH).exist?
+      end
+    end
+
+    it "does not make any network requests when accessing non-license data" do
+      Licensed::Sources::Gradle::Dependency.expects(:retrieve_license).never
+      Licensed::Sources::Gradle::Dependency.expects(:load_csv).never
+
+      Dir.chdir fixtures do
+        dep = source.dependencies.detect { |d| d.name == "io.netty:netty-all" }
+        # accessing non-license dependency data does not make network requests
+        dep.name
+        dep.version
+      end
+    end
+  end
+
+  context "with a gradle multi-project" do
+    let(:fixtures) { File.expand_path("../../fixtures/gradle/multi-project", __FILE__) }
 
     it "returns the dependency license" do
       Dir.chdir fixtures do
