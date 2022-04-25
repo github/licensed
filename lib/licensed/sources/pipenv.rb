e7988ad080254c651739ee9fa53e950189c38888
Jon Ruskin <jonabc@github.com>
2022-04-25 14:37:16 -0700

    support pip packages with license_files folder

--- lib/licensed/sources/pipenv.rb
+++ lib/licensed/sources/pipenv.rb
@@ -4,44 +4,16 @@ require "parallel"
 
 module Licensed
   module Sources
-    class Pipenv < Source
+    class Pipenv < Pip
       def enabled?
         Licensed::Shell.tool_available?("pipenv") && File.exist?(config.pwd.join("Pipfile.lock"))
       end
 
-      def enumerate_dependencies
-        Parallel.map(pakages_from_pipfile_lock, in_threads: Parallel.processor_count) do |package_name|
-          package = package_info(package_name)
-          location = File.join(package["Location"], package["Name"].gsub("-", "_") +  "-" + package["Version"] + ".dist-info")
-          Dependency.new(
-            name: package["Name"],
-            version: package["Version"],
-            path: location,
-            metadata: {
-              "type"        => Pipenv.type,
-              "summary"     => package["Summary"],
-              "homepage"    => package["Home-page"]
-            }
-          )
-        end
-      end
+      protected
 
-      private
-
-      def pakages_from_pipfile_lock
-        Licensed::Shell.execute("pipenv", "run", "pip", "list")
-            .lines
-            .drop(2)  # Header
-            .map { |line| line.strip.split.first.strip }
-      end
-
-      def package_info(package_name)
-        p_info = Licensed::Shell.execute("pipenv", "run", "pip", "--disable-pip-version-check", "show", package_name).lines
-        p_info.each_with_object(Hash.new(0)) { |pkg, a|
-          k, v = pkg.split(":", 2)
-          next if k.nil? || k.empty?
-          a[k.strip] = v&.strip
-        }
+      # Returns the command to run pip
+      def pip_command
+        "pipenv run pip"
       end
     end
   end
