8a63e37f6f1f48a2a3bf8ece804a0805ebc57527
Jon Ruskin <jonabc@github.com>
2023-02-08 09:56:21 -0700

    release 4.1.0
    ### Added
    
    - Custom license terms can be added to dependencies via new configuration options (https://github.com/github/licensed/pull/624)
    - Licensed is now integrated with pnpm to enumerate dependencies (https://github.com/github/licensed/pull/626)

--- lib/licensed/version.rb
+++ lib/licensed/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Licensed
-  VERSION = "4.0.4".freeze
+  VERSION = "4.1.0".freeze
 
   def self.previous_major_versions
     major_version = Gem::Version.new(Licensed::VERSION).segments.first
