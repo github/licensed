f55ac3e2b22d61317ffa76b39b1e18ab3e9b7da9
Jon Ruskin <jonabc@github.com>
2023-02-20 15:05:49 -0700

    don't run command source tests on new test source

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,7 @@ require "test_helpers/test_source"
 def each_source(&block)
   Licensed::Sources::Source.sources.each do |source_class|
     # don't run tests meant for actual dependency enumerators on the test source
-    next if source_class == TestSource
+    next if source_class <= TestSource
 
     # if a specific source type is set via ENV, skip other source types
     next if ENV["SOURCE"] && source_class.full_type != ENV["SOURCE"].downcase
