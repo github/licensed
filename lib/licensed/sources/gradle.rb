c5c5c2d45375fa25b26edc6af1f32ba5b529ee5b
Jon Ruskin <jonabc@github.com>
2023-02-21 15:57:07 -0700

    update configuration usage in init script 1. runtime -> runtimeOnly 2. create custom configurations that extend from   the real configurations being used to work around   Gradle core configurations not being resolvable

--- lib/licensed/sources/gradle.rb
+++ lib/licensed/sources/gradle.rb
@@ -9,7 +9,7 @@ require "fileutils"
 module Licensed
   module Sources
     class Gradle < Source
-      DEFAULT_CONFIGURATIONS   = ["runtime", "runtimeClasspath"].freeze
+      DEFAULT_CONFIGURATIONS   = ["runtimeOnly", "runtimeClasspath"].freeze
       GRADLE_LICENSES_PATH     = ".gradle-licenses".freeze
       GRADLE_LICENSES_CSV_NAME = "licenses.csv".freeze
       class Dependency < Licensed::Dependency
@@ -150,12 +150,17 @@ module Licensed
         private
 
         def create_init_script(configurations)
+          # we need to create extensions in the event that the user hasn't configured custom configurations
+          # to avoid hitting errors where core Gradle configurations are set with canBeResolved=false 
+          configuration_map = configurations.map { |c| [c, "licensed#{c}"] }.to_h
+          configuration_dsl = configuration_map.map { |orig, custom| "#{custom}.extendsFrom(#{orig})" }
+
           f = Tempfile.new(["init", ".gradle"])
           f.write(
             <<~EOF
                 import com.github.jk1.license.render.CsvReportRenderer
                 import com.github.jk1.license.filter.LicenseBundleNormalizer
-                final configs = #{configurations.inspect}
+                final configs = #{configuration_map.values.inspect}
 
                 initscript {
                   repositories {
@@ -169,6 +174,10 @@ module Licensed
                 }
 
                 allprojects {
+                  configurations {
+                    #{configuration_dsl.join("\n") }
+                  }
+
                   apply plugin: com.github.jk1.license.LicenseReportPlugin
                   licenseReport {
                       outputDir = "$rootDir/.gradle-licenses"
