1bc9dbdff6269dce736ee0f284cfc3ee87eade4e
Kevin Dangoor <dangoor@github.com>
2024-08-06 09:34:47 -0400

    Bump version to 4.5 (#730)

--- lib/licensed/version.rb
+++ lib/licensed/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Licensed
-  VERSION = "4.4.0".freeze
+  VERSION = "4.5.0".freeze
 
   def self.previous_major_versions
     major_version = Gem::Version.new(Licensed::VERSION).segments.first
